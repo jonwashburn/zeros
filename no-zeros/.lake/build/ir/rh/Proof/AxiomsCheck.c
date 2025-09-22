824efdca246544dff34d1c292d925831a0be7b01
Jonathan Washburn <jonathanwashburn@unknown4aae307223b4.attlocal.net>
2025-09-21 22:49:56 -0500

    chore: stage local changes before rebase

--- no-zeros/.lake/build/ir/rh/Proof/AxiomsCheck.c
+++ no-zeros/.lake/build/ir/rh/Proof/AxiomsCheck.c
@@ -1,6 +1,6 @@
 // Lean compiler output
 // Module: rh.Proof.AxiomsCheck
-// Imports: Init Lean rh.Proof.Export
+// Imports: Init Lean rh.Proof.Export rh.Proof.CRClosure rh.Proof.Entry
 #include <lean/lean.h>
 #if defined(__clang__)
 #pragma clang diagnostic ignored "-Wunused-parameter"
@@ -16,6 +16,8 @@ extern "C" {
 lean_object* initialize_Init(uint8_t builtin, lean_object*);
 lean_object* initialize_Lean(uint8_t builtin, lean_object*);
 lean_object* initialize_rh_Proof_Export(uint8_t builtin, lean_object*);
+lean_object* initialize_rh_Proof_CRClosure(uint8_t builtin, lean_object*);
+lean_object* initialize_rh_Proof_Entry(uint8_t builtin, lean_object*);
 static bool _G_initialized = false;
 LEAN_EXPORT lean_object* initialize_rh_Proof_AxiomsCheck(uint8_t builtin, lean_object* w) {
 lean_object * res;
@@ -28,6 +30,12 @@ res = initialize_Lean(builtin, lean_io_mk_world());
 if (lean_io_result_is_error(res)) return res;
 lean_dec_ref(res);
 res = initialize_rh_Proof_Export(builtin, lean_io_mk_world());
+if (lean_io_result_is_error(res)) return res;
+lean_dec_ref(res);
+res = initialize_rh_Proof_CRClosure(builtin, lean_io_mk_world());
+if (lean_io_result_is_error(res)) return res;
+lean_dec_ref(res);
+res = initialize_rh_Proof_Entry(builtin, lean_io_mk_world());
 if (lean_io_result_is_error(res)) return res;
 lean_dec_ref(res);
 return lean_io_result_mk_ok(lean_box(0));
